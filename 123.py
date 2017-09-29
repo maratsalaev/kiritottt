eba870802453f0fb17cc59b24c0ce30c24d5a379
maratsalaev <32392872+maratsalaev@users.noreply.github.com>
2017-09-29 17:02:38 +0300

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,43 @@
+# # a = 5 
+# # print(a)
+# # print(a + 5)
+# # print(a)
+# # a = 15
+# # print(a)
+# # aer2er = 67
+# # print(aer2er)
+# # храмасома = 48 - так не надо делать
+# # print(храмасома)
+# seconDnumber = 67
+# # print(seconDnumber)
+# # seconDnumber = seconDnumber + 5
+# # seconDnumber = seconDnumber * 5 
+# # seconDnumber = seconDnumber - 5
+# # seconDnumber += 5
+# print(seconDnumber) 
+# seconDnumber /= 5
+# print(seconDnumber) 
+# seconDnumber -= 5 
+# print(seconDnumber) 
+# seconDnumber *= 5
+# print(seconDnumber) 
+#text = "амин сьел амина"
+#print(text + "!")
+# text = input("ввидите текст")
+# print(text + "!!!")
+
+# input-возращает только стр
+# print(int("324"))
+# print(int(5.5))
+# print(int(True))
+
+#print("гаджи" + str(1))
+#print(float("1"))
+# name1 = input("введите 1 имя: ")
+# name2 = input("введите 2 имя: ")
+# print(name1 + " + " + name2 + "= дружба!")
+# a = int(input("первое число"))
+# b = int(input("второе  число"))
+# c =(a+b)/2
+# print("средние арифметичекое:",c)
+print("тимур", "арслан", 132)
